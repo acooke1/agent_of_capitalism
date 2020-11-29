3788f64c354e06794341ca6b9d2db85e34e7c86d
Maxime <maxime_hendrikse_liu@brown.edu>
2020-11-28 22:03:16 -0500

    Added num_steps_to_coin()
    Now reward for moving into an empty space is based on the number of steps to the nearest coin

--- game_level.py
+++ game_level.py
@@ -12,7 +12,7 @@ import copy
 levels = [
     [ # this first level is probably a bad level to try to use an enemy on: lots of choke points to get stuck in, hard to get around the enemy
         [-.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1],
-        [-.1, 0.1, 0.0, 0.0, 1.0, 0.0, 0.0, -.1],
+        [-.1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -.1],
         [-.1, -.1, -.1, 0.0, -.1, 0.0, 0.0, -.1],
         [-.1, 1.0, 0.0, 0.0, -.1, 1.0, 1.0, -.1],
         [-.1, -.1, 0.0, 0.0, -.1, -.1, -.1, -.1],
@@ -20,9 +20,9 @@ levels = [
         [-.1, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, -.1],
         [-.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1]
     ],
-    [ # TODO if we implement the enemy, we can swap the bottom-right coin for the enemy
+    [
         [-.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1],
-        [-.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, -.1],
+        [-.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, -.1],
         [-.1, 0.0, -.1, -.1, -.1, 0.0, -.1, 0.0, 0.0, -.1, 0.0, -.1, -.1, -.1, 0.0, -.1],
         [-.1, 0.0, -.1, 1.0, 0.0, 0.0, -.1, 0.0, 0.0, -.1, 0.0, 0.0, 1.0, -.1, 0.0, -.1],
         [-.1, 0.0, -.1, 0.0, 0.0, 0.0, -.1, 0.0, 0.0, -.1, 0.0, 0.0, 0.0, -.1, 0.0, -.1],
@@ -39,7 +39,7 @@ levels = [
         [-.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1, -.1]
     ]
 ]
-level_num_coins = [8, 11] # TODO tweak these values if we add an enemy and remove a coin
+level_num_coins = [8, 11]
 
 
 class GameLevel():
@@ -60,16 +60,17 @@ class GameLevel():
         self.enemy_running_into_wall_factor = 0.9 # Higher values means less likely it will run into a wall
         self.enemy_pursuing_player_factor = 0.5 # Higher values means greater weight on pursuing the player
 
-        # Space types
-        self.empty_level_val = 0.0
-        self.wall_level_val = -0.1
-        self.coin_level_val = 1.0
-        self.player_level_val = 0.1
-        self.enemy_level_val = -1.0
+        # What walls/coins/etc. look like, to the model
+        # DO NOT CHANGE, or else the model just won't work
+        self.empty_level_val = 0.0 # DO NOT CHANGE
+        self.wall_level_val = -0.1 # DO NOT CHANGE
+        self.coin_level_val = 1.0 # DO NOT CHANGE
+        self.player_level_val = 0.1 # DO NOT CHANGE
+        self.enemy_level_val = -1.0 # DO NOT CHANGE
 
         # Reward values
         # TODO tweak these values
-        self.empty_space_reward = 0.01
+        self.empty_space_reward = 0.01 # NOTE: THIS VALUE IS NO LONGER USED - EMPTY SPACE REWARD IS NOW CALCULATED BASED ON STEPS TO NEAREST COIN
         self.hit_wall_reward = -0.1
         self.get_coin_reward = 0.5
         self.get_all_coins_reward = 1.0
@@ -81,14 +82,15 @@ class GameLevel():
         self.max_steps = self.state_size * 2
 
     def reset_level(self):
-        #print("before")
-        #self.print_map()
         self.level_map = copy.deepcopy(levels[self.level_num])
-        #print("after")
-        #self.print_map()
         self.num_coins_left = copy.deepcopy(level_num_coins[self.level_num])
-        #print(self.coinsLeft(2))
+        #print(self.coinsLeft(self.coin_level_val))
         self.player_pos = [1,1] # Index corresponding to the player's current location in the map
+
+        # TODO TESTING STARTING IN THE MIDDLE
+        self.player_pos = [len(self.level_map)//2, len(self.level_map)//2]
+        self.level_map[self.player_pos[0]][self.player_pos[1]] = self.player_level_val
+
         self.step_num = 0
         if self.has_enemy:
             self.enemy_pos = [len(self.level_map)-2, len(self.level_map)-2]
@@ -96,11 +98,13 @@ class GameLevel():
             self.enemy_alive = True
             self.enemy_over_coin = True
 
+    """
     def coinsLeft(self, coin_value):
         total = 0
         for i in range(len(self.level_map)):
             total+=self.level_map[i].count(coin_value)
         return total
+    """
 
     def step(self, action):
         """
@@ -130,7 +134,17 @@ class GameLevel():
         if action<self.num_direcs:
             # Move validation: also determines reward in this step
             if goal_pos_contents == self.empty_level_val: # moving into an empty space
-                reward = self.empty_space_reward
+                # TODO tried swapping for the numsteps-based reward!
+                old_pos_reward = self.get_coin_reward / self.num_steps_to_coin(self.player_pos)
+                new_pos_reward = self.get_coin_reward / self.num_steps_to_coin(goal_pos)
+                # With this calculation, the highest new_pos_reward for an empty space is the value of getting a coin
+                # And if it was moving closer to the coin, then old_pos_reward should be half the value of getting the coin
+                reward = new_pos_reward-old_pos_reward
+                # ideally, new_pos_reward should be greater than old_pos_reward:
+                # if not, the reward should be negative, for moving away from the nearest coin
+                
+                # TODO old reward value belos
+                # reward = self.empty_space_reward
             if goal_pos_contents == self.wall_level_val: # moving into a wall
                 reward = self.hit_wall_reward
                 goal_pos = self.player_pos # The player can't move into a wall, so it stays stationary
@@ -216,10 +230,56 @@ class GameLevel():
 
         return [self.level_map, reward, done]
 
+    def num_steps_to_coin(self, input_pos):
+        """
+        Finds the number of spaces from the player to the nearest coin.
+        Essentially a breadth-first-search.
+        :input_pos: a length-2 list of integers, indicating the position to start from
+        :returns: an integer number of steps to the closest coin
+        """
+        level_width = len(self.level_map)
+        greatest_dist = level_width*2 # ((level_width - 2)**2) + 1
+        # TODO two times the side width theoretically should be the greatest possible distance to any given coin
+        distances = [[greatest_dist for x in range(level_width)] for y in range(level_width)] 
+        open_spaces = []
+        open_spaces.append(input_pos)
+        distances[input_pos[0]][input_pos[1]] = 0
+        min_dist = self.state_size + 1
+
+        while len(open_spaces)>0:
+            """for i in range(8):
+                print(distances[i])
+            """
+
+            cur_pos = open_spaces.pop(0)
+            # Loop through the adjacent spaces
+            for direc in range(self.num_direcs):
+                new_pos = [cur_pos[0]+self.coord_adds[direc][0],cur_pos[1]+self.coord_adds[direc][1]]
+                pos_contents = self.level_map[new_pos[0]][new_pos[1]]
+                pos_visited = (distances[new_pos[0]][new_pos[1]] < greatest_dist)
+
+                # If it's a coin, then it must be the closest coin: we're done
+                if pos_contents == self.coin_level_val:
+                    min_dist = distances[cur_pos[0]][cur_pos[1]]+1
+                    return min_dist # TODO THIS MAY BE BAD PRACTICE
+                elif not pos_contents == self.wall_level_val:
+                    # If it's not a coin and not a wall, then we want to check the next available and unvisited spaces
+                    if not pos_visited:
+                        open_spaces.append(new_pos)
+                    # Compare path length to this space through cur_pos
+                    new_dist = distances[cur_pos[0]][cur_pos[1]]+1
+                    if new_dist < distances[new_pos[0]][new_pos[1]]:
+                        # Update the new shortest distance to that path
+                        distances[new_pos[0]][new_pos[1]] = new_dist
+
+        print("Error in the dist_to_coin function: code should never have reached this point.")
+        return None
+
 
     def print_map(self):
         """
         Method should print the currently-stored self.level_map to console
+        :returns: None
         """
         print("Step number " + str(self.step_num))
         num_to_char = [" ", "█", "❂", "♀", "☿"] # Want to go back to the snowman? He's here → ☃
@@ -236,29 +296,35 @@ class GameLevel():
                     char_to_add = num_to_char[2]
                 elif space_contents == self.player_level_val:
                     char_to_add = num_to_char[3]
-                elif space_contents == self.wall_level_val:
+                elif space_contents == self.enemy_level_val:
                     char_to_add = num_to_char[4]
                 print_string += char_to_add
             print(print_string)
 
 
 # = = = = TEST CODE = = = =
-
+"""
 def main():
-    pass
-    # level = GameLevel(0)
-    # level.print_map()
-    # level.step(2)
-    # level.print_map()
-    # level.step(2)
-    # level.print_map()
-    # level.step(2)
-    # level.print_map()
-    # level.step(0)
-    # level.print_map()
-    # level.step(3)
-    # level.print_map()
+    level = GameLevel(0)
+    level.print_map()
+    print("Distance to coin: " + str(level.num_steps_to_coin(level.player_pos)))
+    level.step(2)
+    level.print_map()
+    print("Distance to coin: " + str(level.num_steps_to_coin(level.player_pos)))
+    level.step(2)
+    level.print_map()
+    print("Distance to coin: " + str(level.num_steps_to_coin(level.player_pos)))
+    level.step(2)
+    level.print_map()
+    print("Distance to coin: " + str(level.num_steps_to_coin(level.player_pos)))
+    level.step(0)
+    level.print_map()
+    print("Distance to coin: " + str(level.num_steps_to_coin(level.player_pos)))
+    level.step(3)
+    level.print_map()
+    print("Distance to coin: " + str(level.num_steps_to_coin(level.player_pos)))
 
 
 if __name__ == '__main__':
     main()
+"""
